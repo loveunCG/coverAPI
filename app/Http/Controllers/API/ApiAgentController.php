d51fff4c483ee7f66f0dd2c88994f0796bbd9d94
Hitaro1985 <37762957+Hitaro1985@users.noreply.github.com>
2018-04-11 08:20:28 -0700

    Update ApiAgentController.php

--- app/Http/Controllers/API/ApiAgentController.php
+++ app/Http/Controllers/API/ApiAgentController.php
@@ -34,7 +34,6 @@ class ApiAgentController extends Controller
 		                   * cos(radians(users.longitude) - radians(" . $lon . "))
 		                 + sin(radians(" . $lat . "))
 		                 * sin(radians(users.latitude))) AS distance"))
-                            ->leftJoin('assignJobs', 'users.id', '=', 'assignJobs.agent_id')->orWhere('assignJobs.agent_id', '=', null)
                             ->where(['users.usertype' => 'agent', 'users.isAvailable' => 1])
                             ->orderBy('distance', 'desc')
                             ->take(3)
