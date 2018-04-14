05addc56e91bd1f557aa7908f7d6bca666b0eb64
chalove <loveun1988@outlook.com>
2018-04-14 20:04:48 +0900

    this is cms update

--- app/Http/Controllers/Web/AdminController.php
+++ app/Http/Controllers/Web/AdminController.php
@@ -2,177 +2,163 @@
 
 namespace App\Http\Controllers\Web;
 
-use Illuminate\Http\Request;
+use App\Admin;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
+use Hash;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-use App\Admin;
-use Hash;
 
-class AdminController extends Controller
-{
-    public function getAdminTableInfo(Request $request)
-    {
-        $adminData = Admin::all();
-        $i = 1;
-        $admin_list = [];
-        if (count($adminData) > 0) {
-            foreach ($adminData as $admin) {
-                if ($admin->admin_level == 1) {
-                    continue;
-                }
-                $admin_id = '<input type="hidden" class="adminId" value="' . $admin->id . '">';
-                if ($admin->admin_level == 1) {
-                    $admin_level = '<button type="button" class="btn btn-info-alt waves-effect">Super Admin</button>';
-                } elseif ($admin->admin_level == 2) {
-                    $admin_level = '<button type="button" class="btn btn-primary-alt waves-effect">job manager</button>';
-                } else {
-                    $admin_level = '<button type="button" class="btn btn-inverse-alt waves-effect">user manager</button>';
-                }
-                if ($admin->usr_status==1) {
-                    $adminStatus = '<a href="#" class="btn btn-success"><i class="fa fa-circle"></i></a>';
-                } else {
-                    $adminStatus = '<a href="#" disabled class="btn btn-success"><i class="fa fa-circle-o"></i></a>';
-                }
-                $admin_list['data'][] = array(
-                    $i++ . $admin_id,
-                    $admin->name,
-                    $admin->email,
-                    $admin_level,
-                    $admin->created_at->format("Y-m-d h:i:s"),
-                    $adminStatus
-                );
-            }
-        } else {
-            $admin_list['data'][] = array(
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                ''
-            );
-        }
-        return response()->json($admin_list);
-    }
+class AdminController extends Controller {
+	public function getAdminTableInfo(Request $request) {
+		$adminData = Admin::all();
+		$i = 1;
+		$admin_list = [];
+		if (count($adminData) > 0) {
+			foreach ($adminData as $admin) {
+				if ($admin->admin_level == 1) {
+					continue;
+				}
+				$admin_id = '<input type="hidden" class="adminId" value="' . $admin->id . '">';
+				if ($admin->admin_level == 1) {
+					$admin_level = '<button type="button" class="btn btn-info-alt waves-effect">Super Admin</button>';
+				} elseif ($admin->admin_level == 2) {
+					$admin_level = '<button type="button" class="btn btn-primary-alt waves-effect">job manager</button>';
+				} else {
+					$admin_level = '<button type="button" class="btn btn-inverse-alt waves-effect">user manager</button>';
+				}
+				if ($admin->usr_status == 1) {
+					$adminStatus = '<a href="#" class="btn btn-success"><i class="fa fa-circle"></i></a>';
+				} else {
+					$adminStatus = '<a href="#" disabled class="btn btn-success"><i class="fa fa-circle-o"></i></a>';
+				}
+				$admin_list['data'][] = array(
+					$i++ . $admin_id,
+					$admin->name,
+					$admin->email,
+					$admin_level,
+					$admin->created_at->format("Y-m-d h:i:s"),
+					$adminStatus,
+				);
+			}
+		} else {
+			$admin_list['data'][] = array(
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+			);
+		}
+		return response()->json($admin_list);
+	}
 
-    public function getAdminInfo($adminId = null)
-    {
-        if (!$adminId) {
-            return response()->json();
-        }
-        $adminIfo = Admin::where('id', $adminId)->first();
-        $sendData = array('id'=>$adminIfo->id,
-        'name'=>$adminIfo->name,
-        'email'=>$adminIfo->email,
-        'usr_status'=>$adminIfo->usr_status,
-        'admin_level'=>$adminIfo->admin_level);
-        return response()->json($sendData);
-    }
+	public function getAdminInfo($adminId = null) {
+		if (!$adminId) {
+			return response()->json();
+		}
+		$adminIfo = Admin::where('id', $adminId)->first();
+		$sendData = array('id' => $adminIfo->id,
+			'name' => $adminIfo->name,
+			'email' => $adminIfo->email,
+			'usr_status' => $adminIfo->usr_status,
+			'admin_level' => $adminIfo->admin_level);
+		return response()->json($sendData);
+	}
 
-    public function duplicationEmail(Request $request)
-    {
-        $email = $request->email;
-        $adminIfo = Admin::where('email', $email)->get();
-        if (count($adminIfo) > 0) {
-            $sendData = array('status'=>true);
-        } else {
-            $sendData = array('status'=>false);
-        }
-        return response()->json($sendData);
-    }
+	public function duplicationEmail(Request $request) {
+		$email = $request->email;
+		$adminIfo = Admin::where('email', $email)->get();
+		if (count($adminIfo) > 0) {
+			$sendData = array('status' => true);
+		} else {
+			$sendData = array('status' => false);
+		}
+		return response()->json($sendData);
+	}
 
-    public function createAdmin(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-                  'email' => 'required|string|email|max:255|unique:admins',
-                  'password' => 'required',
-                  'name' => 'required|string|max:255',
-                  'admin_level' => 'required'
-      ]);
-        if ($validator->fails()) {
-            return response()->json(['message' => 'Submit error', 'data' => $validator->errors(), 'response_code' => 0], 200);
-        }
-        try {
-            $admin = array(
-              'email'=>$request->email,
-              'password'=>bcrypt($request->password),
-              'admin_level'=>$request->admin_level,
-              'name'=>$request->name,
-              'devicename'=>'chrome',
-              'usertype' => 'admin',
-              'usr_status' => 1
-            );
-            Admin::create($admin);
-            return response()->json(['message' => 'Create admin successfully!', 'data' => null, 'response_code' => 1], 200);
-        } catch (\Exception $e) {
-            return response()->json(['message' => 'Can not create admin account!', 'data' => $e, 'response_code' => 0], 200);
-        }
-    }
+	public function createAdmin(Request $request) {
+		$validator = Validator::make($request->all(), [
+			'email' => 'required|string|email|max:255|unique:admins',
+			'password' => 'required',
+			'name' => 'required|string|max:255',
+			'admin_level' => 'required',
+		]);
+		if ($validator->fails()) {
+			return response()->json(['message' => 'Submit error', 'data' => $validator->errors(), 'response_code' => 0], 200);
+		}
+		try {
+			$admin = array(
+				'email' => $request->email,
+				'password' => bcrypt($request->password),
+				'admin_level' => $request->admin_level,
+				'name' => $request->name,
+				'devicename' => 'chrome',
+				'usertype' => 'admin',
+				'usr_status' => 1,
+			);
+			Admin::create($admin);
+			return response()->json(['message' => 'Create admin successfully!', 'data' => null, 'response_code' => 1], 200);
+		} catch (\Exception $e) {
+			return response()->json(['message' => 'Can not create admin account!', 'data' => $e, 'response_code' => 0], 200);
+		}
+	}
 
-    public function updateAdmin(Request $request)
-    {
-        $id = $request->admin_id;
-        $user = Admin::findOrFail($id);
-        $updateData = array('name'=>$request->name, 'admin_level'=>$request->admin_level);
-        $user->update($updateData);
-        return response()->json(array('status'=>'success'));
-    }
+	public function updateAdmin(Request $request) {
+		$id = $request->admin_id;
+		$user = Admin::findOrFail($id);
+		$updateData = array('name' => $request->name, 'admin_level' => $request->admin_level);
+		$user->update($updateData);
+		return response()->json(array('status' => 'success'));
+	}
 
-    public function activate(Request $request)
-    {
-        $updateData = array('usr_status'=>$request->status);
-        $user = Admin::findOrFail($request->adminUpdateID);
-        $user->update($updateData);
-        return response()->json(array('status'=>'success'));
-    }
+	public function activate(Request $request) {
+		$updateData = array('usr_status' => $request->status);
+		$user = Admin::findOrFail($request->adminUpdateID);
+		$user->update($updateData);
+		return response()->json(array('status' => 'success'));
+	}
 
-    public function resetPassword(Request $request)
-    {
-        $updateData = array('password'=>bcrypt($request->password));
-        $user = Admin::findOrFail($request->admin_id);
-        $user->update($updateData);
-        return response()->json(array('status'=>'success'));
-    }
+	public function resetPassword(Request $request) {
+		$updateData = array('password' => bcrypt($request->password));
+		$user = Admin::findOrFail($request->admin_id);
+		$user->update($updateData);
+		return response()->json(array('status' => 'success'));
+	}
 
-    public function removeAdmin(Request $request)
-    {
-        if (Gate::allows('isSuperAdmin')) {
-            return response()->json(array('status'=>'error'));
-        }
-        $user = Admin::findOrFail($request->adminUpdateID);
-        $user->delete();
-        return response()->json(array('status'=>'success'));
-    }
+	public function removeAdmin(Request $request) {
+		if (Gate::allows('isSuperAdmin')) {
+			return response()->json(array('status' => 'error'));
+		}
+		$user = Admin::findOrFail($request->adminUpdateID);
+		$user->delete();
+		return response()->json(array('status' => 'success'));
+	}
 
-    public function changePassword(Request $request)
-    {
-        $user = Auth::getUser();
-        $validator = Validator::make($request->all(), [
-          'old_password' => 'required',
-          'new_password' => 'required|confirmed',
-          'new_password_confirmation' => 'required'
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['message' => 'Submit error', 'data' => $validator->errors(), 'response_code' => 0], 200);
-        }
-        if (Hash::check($request->old_password, $user->password)) {
-            $user->password = bcrypt($request->get('new_password'));
-            $user->save();
-            return response()->json(['message' => 'Reset Password successfully!', 'data' => [], 'response_code' => 1], 200);
-        } else {
-            return response()->json(['message' => 'current password is incorrect!', 'data' => [], 'response_code' => 0], 200);
-        }
-    }
+	public function changePassword(Request $request) {
+		$user = Auth::getUser();
+		$validator = Validator::make($request->all(), [
+			'old_password' => 'required',
+			'new_password' => 'required|confirmed',
+			'new_password_confirmation' => 'required',
+		]);
+		if ($validator->fails()) {
+			return response()->json(['message' => 'Submit error', 'data' => $validator->errors(), 'response_code' => 0], 200);
+		}
+		if (Hash::check($request->old_password, $user->password)) {
+			$user->password = bcrypt($request->get('new_password'));
+			$user->save();
+			return response()->json(['message' => 'Reset Password successfully!', 'data' => [], 'response_code' => 1], 200);
+		} else {
+			return response()->json(['message' => 'current password is incorrect!', 'data' => [], 'response_code' => 0], 200);
+		}
+	}
 
-    public function getAuthuserInfo()
-    {
-        return response()->json(['message' => 'success', 'data' => Auth::getUser(), 'response_code' => 1], 200);
-    }
-
-    //
+	public function getAuthuserInfo() {
+		return response()->json(['message' => 'success', 'data' => Auth::getUser(), 'response_code' => 1], 200);
+	}
 }
--- app/Http/Controllers/Web/JobsController.php
+++ app/Http/Controllers/Web/JobsController.php
@@ -2,253 +2,246 @@
 
 namespace App\Http\Controllers\web;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Model\JobsModel;
-use App\Model\InsuranceModel;
 use App\Model\AssignJob;
-use App\Model\QuotationModel;
 use App\Model\DocumentsModel;
+use App\Model\InsuranceModel;
+use App\Model\JobsModel;
+use App\Model\QuotationModel;
 use App\User;
+use Illuminate\Http\Request;
 
-class JobsController extends Controller
-{
-    public function getJobList(Request $request)
-    {
-        $jobs = JobsModel::all();
-        $i = 1;
-        try {
-            if (count($jobs) > 0) {
-                foreach ($jobs as $job) {
-                    if ($job->job_status==1) {
-                        $jobStatus = '<a href="#" class="btn btn-primary-alt">Completed</a>';
-                    } elseif ($job->job_status==0) {
-                        $jobStatus = '<a href="#" class="btn btn-warning-alt">In progreesing</a>';
-                    } else {
-                        $jobStatus = '<a href="#" class="btn btn-inverse-alt">Rejected</a>';
-                    }
-                    $job_id = '<input type="hidden" class="job_id" value="' . $job->id . '">';
-                    try {
-                        $insurance_name = InsuranceModel::findOrFail($job->insurance_type)->insurance_name;
-                        $user_name = User::findOrFail($job->user_id)->username;
-                    } catch (\Exception $e) {
-                        $user_name = "undefinded";
-                        $insurance_name = "undefinded";
-                    }
-                    $job_list['data'][] = array(
-                      $i++.$job_id,
-                      $job->name,
-                      $job->nric,
-                      $insurance_name,
-                      $job->indicative_sum,
-                      $job->address,
-                      $job->postcode,
-                      $job->state,
-                      $job->expired_date,
-                      $user_name,
-                      $jobStatus,
-                    );
-                }
-            } else {
-                $job_list['data'][0] = array(
-                ' ',
-                ' ',
-                ' ',
-                ' ',
-                ' ',
-                ' ',
-                ' ',
-                ' ',
-                ' ',
-                ' ',
-                ' '
-              );
-            }
-        } catch (\Exception $e) {
-            $job_list['data'][0] = array(
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' ',
-            ' '
-          );
-            return response()->json($job_list);
-        }
-        return response()->json($job_list);
-    }
+class JobsController extends Controller {
+	public function getJobList(Request $request) {
+		$jobs = JobsModel::all();
+		$i = 1;
+		try {
+			if (count($jobs) > 0) {
+				foreach ($jobs as $job) {
+					if ($job->job_status == 1) {
+						$jobStatus = '<a href="#" class="btn btn-primary-alt">Completed</a>';
+					} elseif ($job->job_status == 0) {
+						$jobStatus = '<a href="#" class="btn btn-warning-alt">In progreesing</a>';
+					} else {
+						$jobStatus = '<a href="#" class="btn btn-inverse-alt">Rejected</a>';
+					}
+					$job_id = '<input type="hidden" class="job_id" value="' . $job->id . '">';
+					try {
+						$insurance_name = InsuranceModel::findOrFail($job->insurance_type)->insurance_name;
+						$user_name = User::findOrFail($job->user_id)->username;
+					} catch (\Exception $e) {
+						$user_name = "undefinded";
+						$insurance_name = "undefinded";
+					}
+					$job_list['data'][] = array(
+						$i++ . $job_id,
+						$job->name,
+						$job->nric,
+						$insurance_name,
+						$job->indicative_sum,
+						$job->address,
+						$job->postcode,
+						$job->state,
+						$job->expired_date,
+						$user_name,
+						$jobStatus,
+					);
+				}
+			} else {
+				$job_list['data'][0] = array(
+					' ',
+					' ',
+					' ',
+					' ',
+					' ',
+					' ',
+					' ',
+					' ',
+					' ',
+					' ',
+					' ',
+				);
+			}
+		} catch (\Exception $e) {
+			$job_list['data'][0] = array(
+				' ',
+				' ',
+				' ',
+				' ',
+				' ',
+				' ',
+				' ',
+				' ',
+				' ',
+				' ',
+				' ',
+			);
+			return response()->json($job_list);
+		}
+		return response()->json($job_list);
+	}
 
-    public function removeJob(Request $request)
-    {
-        if ($request->has('job_id')) {
-            $job_id = $request->job_id;
-            try {
-                JobsModel::findOrFail($job_id)->delete();
-                return response()->json(['message' => 'Delete successfully!', 'data' => null, 'response_code' => 1], 200);
-            } catch (\Exception $e) {
-                return response()->json(['message' => 'Delete failed!', 'data' => null, 'response_code' => 0], 500);
-            }
-        } else {
-            return response()->json(['message' => 'Please insert Job ID', 'data' => null, 'response_code' => 0], 200);
-        }
-    }
+	public function removeJob(Request $request) {
+		if ($request->has('job_id')) {
+			$job_id = $request->job_id;
+			try {
+				JobsModel::findOrFail($job_id)->delete();
+				return response()->json(['message' => 'Delete successfully!', 'data' => null, 'response_code' => 1], 200);
+			} catch (\Exception $e) {
+				return response()->json(['message' => 'Delete failed!', 'data' => null, 'response_code' => 0], 500);
+			}
+		} else {
+			return response()->json(['message' => 'Please insert Job ID', 'data' => null, 'response_code' => 0], 200);
+		}
+	}
 
-    public function JobDetailView(Request $request)
-    {
-        if ($request->has('job_id')) {
-            try {
-                $job_id = $request->job_id;
-                $job_data = JobsModel::findOrFail($job_id);
-                $user = $job_data->users;
-                $assignedJob = $job_data->assignedJob;
-                $quotatioin = $job_data->quotations;
-                return response()->json([
-                  'message' => 'successfully!',
-                  'data' => $job_data,
-                  'response_code' => 1
-                ], 200);
-            } catch (\Exception $e) {
-                return response()->json(['message' => 'Can not get job Data!', 'data' => $e, 'response_code' => 0], 500);
-            }
-        } else {
-            return response()->json(['message' => 'job_id is required', 'data' => null, 'response_code' => 0], 200);
-        }
-    }
+	public function JobDetailView(Request $request) {
+		if ($request->has('job_id')) {
+			try {
+				$job_id = $request->job_id;
+				$job_data = JobsModel::findOrFail($job_id);
+				$user = $job_data->users;
+				$assignedJob = $job_data->assignedJob;
+				$quotatioin = $job_data->quotations;
+				return response()->json([
+					'message' => 'successfully!',
+					'data' => $job_data,
+					'response_code' => 1,
+				], 200);
+			} catch (\Exception $e) {
+				return response()->json(['message' => 'Can not get job Data!', 'data' => $e, 'response_code' => 0], 500);
+			}
+		} else {
+			return response()->json(['message' => 'job_id is required', 'data' => null, 'response_code' => 0], 200);
+		}
+	}
 
-    public function getQuotationList(Request $request)
-    {
-        $send_data = [];
+	public function getQuotationList(Request $request) {
+		$send_data = [];
 
-        if ($job_id = $request->job_id) {
-            $quotations = QuotationModel::where('job_id', $job_id)->get();
-        } else {
-            $quotations = QuotationModel::all();
-        }
-        try {
-            if (count($quotations)>0) {
-                $i = 1;
-                foreach ($quotations as $quotation) {
-                    $send_data['data'][]=[
-                    $i++,
-                    $quotation->agent->username,
-                    $quotation->quotation_price,
-                    $quotation->quotation_description,
-                    $quotation->jobs->job_status
-                  ];
-                }
-            } else {
-                $send_data['data'][0]=[
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                ''
-              ];
-            }
-        } catch (\Exception $e) {
-            $send_data['data'][0]=[
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            '',
-            ''
-          ];
-        }
-        return response()->json($send_data);
-    }
+		if ($job_id = $request->job_id) {
+			$quotations = QuotationModel::where('job_id', $job_id)->get();
+		} else {
+			$quotations = QuotationModel::all();
+		}
+		try {
+			if (count($quotations) > 0) {
+				$i = 1;
+				foreach ($quotations as $quotation) {
+					$send_data['data'][] = [
+						$i++,
+						$quotation->agent->username,
+						$quotation->quotation_price,
+						$quotation->quotation_description,
+						$quotation->jobs->job_status,
+					];
+				}
+			} else {
+				$send_data['data'][0] = [
+					'',
+					'',
+					'',
+					'',
+					'',
+					'',
+					'',
+					'',
+				];
+			}
+		} catch (\Exception $e) {
+			$send_data['data'][0] = [
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+				'',
+			];
+		}
+		return response()->json($send_data);
+	}
 
-    public function getDocumentList(Request $request)
-    {
-        $send_document_list = [];
-        if ($request->has('job_id')) {
-            $document_lists = DocumentsModel::where('job_id', $request->job_id)->get();
-        } else {
-            $document_lists = DocumentsModel::all();
-        }
-        if (count($document_lists) > 0) {
-            $index = 1;
-            try {
-                foreach ($document_lists as $document) {
-                    $send_document_list['data'][] = [
-                    $index++,
-                    $document->customer->username,
-                    $document->document
-                  ];
-                }
-            } catch (\Exception $e) {
-                $send_document_list['data'][0] =[
-                '',
-                '',
-                '',
-                '',
-              ];
-            }
-        } else {
-            $send_document_list['data'][0] =[
-            '',
-            '',
-            '',
-            '',
-          ];
-        }
-        return response()->json($send_document_list);
-    }
+	public function getDocumentList(Request $request) {
+		$send_document_list = [];
+		if ($request->has('job_id')) {
+			$document_lists = DocumentsModel::where('job_id', $request->job_id)->get();
+		} else {
+			$document_lists = DocumentsModel::all();
+		}
+		if (count($document_lists) > 0) {
+			$index = 1;
+			try {
+				foreach ($document_lists as $document) {
+					$send_document_list['data'][] = [
+						$index++,
+						$document->customer->username,
+						$document->document,
+					];
+				}
+			} catch (\Exception $e) {
+				$send_document_list['data'][0] = [
+					'',
+					'',
+					'',
+					'',
+				];
+			}
+		} else {
+			$send_document_list['data'][0] = [
+				'',
+				'',
+				'',
+				'',
+			];
+		}
+		return response()->json($send_document_list);
+	}
 
-    public function getAssignedJobList(Request $request)
-    {
-        $send_assignedList = [];
-        if ($request->has('job_id')) {
-            $assignLists = AssignJob::where('job_id', $request->job_id)->get();
-        } else {
-            $assignLists = AssignJob::all();
-        }
-        if (count($assignLists) > 0) {
-            $index = 1;
-            try {
-                foreach ($assignLists as $job) {
-                    if ($job->jobstatus == 1) {
-                        $status = '<a href="#" class="btn btn-primary-alt">Accepted</a>';
-                    } elseif ($job->jobstatus == 2) {
-                        $status = '<a href="#" class="btn btn-primary-alt">Rejected</a>';
-                    } elseif ($job->jobstatus == 3) {
-                        $status = '<a href="#" class="btn btn-primary-alt">Completed</a>';
-                    } else {
-                        $status = '<a href="#" class="btn btn-primary-alt">Assigning</a>';
-                    }
-                    $send_assignedList['data'][] = [
-                      $index++,
-                      User::findOrFail($job->customer_id)->username,
-                      User::findOrFail($job->agent_id)->username,
-                      $status
-                    ];
-                }
-            } catch (\Exception $e) {
-                $send_assignedList['data'][0] =[
-                  '',
-                  '',
-                  '',
-                  '',
-                ];
-            }
-        } else {
-            $send_assignedList['data'][0] =[
-              '',
-              '',
-              '',
-              '',
-            ];
-        }
-        return response()->json($send_assignedList);
-    }
+	public function getAssignedJobList(Request $request) {
+		$send_assignedList = [];
+		if ($request->has('job_id')) {
+			$assignLists = AssignJob::where('job_id', $request->job_id)->get();
+		} else {
+			$assignLists = AssignJob::all();
+		}
+		if (count($assignLists) > 0) {
+			$index = 1;
+			try {
+				foreach ($assignLists as $job) {
+					if ($job->jobstatus == 1) {
+						$status = '<a href="#" class="btn btn-primary-alt">Accepted</a>';
+					} elseif ($job->jobstatus == 2) {
+						$status = '<a href="#" class="btn btn-primary-alt">Rejected</a>';
+					} elseif ($job->jobstatus == 3) {
+						$status = '<a href="#" class="btn btn-primary-alt">Completed</a>';
+					} else {
+						$status = '<a href="#" class="btn btn-primary-alt">Assigning</a>';
+					}
+					$send_assignedList['data'][] = [
+						$index++,
+						User::findOrFail($job->customer_id)->username,
+						User::findOrFail($job->agent_id)->username,
+						$status,
+					];
+				}
+			} catch (\Exception $e) {
+				$send_assignedList['data'][0] = [
+					'',
+					'',
+					'',
+					'',
+				];
+			}
+		} else {
+			$send_assignedList['data'][0] = [
+				'',
+				'',
+				'',
+				'',
+			];
+		}
+		return response()->json($send_assignedList);
+	}
 }
