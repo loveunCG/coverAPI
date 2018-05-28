fdf5a8eccb1fe76091b56e9530f9271917d9b809
Hitaro1985 <tomasfans1985@gmail.com>
2018-05-27 21:59:26 -0700

    getQuotation update

--- app/Http/Controllers/API/ApiAgentController.php
+++ app/Http/Controllers/API/ApiAgentController.php
@@ -475,14 +475,13 @@ class ApiAgentController extends Controller
     {
         $user = JWTAuth::parseToken()->authenticate();
         if ($request->has('quotation_id')) {
-            $quotation = QuotationModel::join('jobs', 'jobs.id', '=', 'quotations.job_id')
-                            ->where(['quotations.id' => $request->quotation_id])->get();
+            $quotation = QuotationModel::where(['quotations.id' => $request->quotation_id])->get();
             return response()->json(['message' => 'Get quotation by quotation ID', 'data' => $quotation, 'response_code' => 1], 200);
         } else {
             if ($user->usertype == 'agent') {
-                $quotations = QuotationModel::join('jobs', 'jobs.id', '=', 'quotations.job_id')
+                //$quotations = QuotationModel::join('jobs', 'jobs.id', '=', 'quotations.job_id')
                                 ->where(['quotations.agent_id' => $user->id])->get();
-                return response()->json(['message' => 'Get quotation list by agent id', 'data' => $quotations, 'response_code' => 1], 200);
+                return response()->json(['message' => 'Get quotation list by agent id', 'data' => null, 'response_code' => 1], 200);
             } else {
                 $quotations = JobsModel::join('quotations', 'jobs.id', '=', 'quotations.job_id')
                                 ->join('users', 'quotations.agent_id', '=', 'users.id')
